8e6e56d2042dbeef6824ca1c513adad70ac07b7b
Paweł <pawel_gnat@o2.pl>
2022-06-02 18:08:30 +0200

    working with js and css

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,19 @@ const navMobile = document.querySelector('.nav__items')
 const navBtn = document.querySelector('.nav__button')
 const footerYear = document.querySelector('.footer__year')
 const navLinks = document.querySelectorAll('.nav__link')
+// const menuItems = document.querySelectorAll('a')
+// const scrollSpySections = document.querySelectorAll('.section')
 
 const handleNav = () => {
 	navMobile.classList.toggle('nav__items--active')
 	document.body.classList.toggle('sticky-body')
 
-navLinks.forEach(item => {
-	item.addEventListener('click', () => {
-		navMobile.classList.remove('nav__items--active')
+	navLinks.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav__items--active')
+		})
 	})
-})
 }
-
-navBtn.addEventListener('click', handleNav)
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
@@ -22,3 +22,44 @@ const handleCurrentYear = () => {
 }
 
 handleCurrentYear()
+
+// const handleScrollSpy = () => {
+// 	if (document.body.classList.contains('main-page')) {
+// 		const sections = []
+// 		scrollSpySections.forEach(section => {
+// 			if (window.scrollY <= section.offsetTop + section.offsetHeight) {
+// 				sections.push(section)
+
+// 				const activeSection = document.querySelector(`[href*="${sections[0].id}"]`)
+// 				console.log(sections)
+// 				console.log(activeSection)
+// 				menuItems.forEach(item => item.classList.remove('active'))
+
+// 				activeSection.classList.add('active')
+// 			}
+// 		})
+// 	}
+// }
+
+function scrollspy() {
+	const links = document.querySelectorAll('.nav__link')
+	const aboutUs = document.getElementById('aboutus').offsetTop
+	const offer = document.getElementById('offer').offsetTop
+	const navHeight = document.getElementById('header').offsetTop
+
+	const changeClasses = index => {
+		links.forEach(link => link.classList.remove('active'))
+		links[index].classList.add('active')
+	}
+
+
+	if (window.scrollY < aboutUs - navHeight) {
+		changeClasses(0)
+	} else if (window.scrollY >= aboutUs - navHeight && window.scrollY < offer - navHeight) {
+		changeClasses(1)
+	} else if (window.scrollY >= offer - navHeight) {
+		changeClasses(2)
+	}
+}
+navBtn.addEventListener('click', handleNav)
+// window.addEventListener('scroll', handleScrollSpy)
